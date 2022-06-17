ae8aef2683593601e46a667d134068963601af6b
Key27 <Desertod2014@mail.ua>
2022-06-17 23:51:59 +0300

    Remove unnecessary code

--- Settings.py
+++ Settings.py
@@ -20,14 +20,3 @@ ROOT.geometry(f'{RES[1]}x{RES[0]}')
 
 #ROOT.resizable(width=False, height=False)
 
-# Settings of game interface
-CANVAS = tkinter.Canvas(ROOT, width=RES[1], height=RES[0],\
-                        bg='purple', highlightthickness=0) 
-CANVAS.pack()
-
-name_img_obj = tkinter.PhotoImage(file='img/Tetris_logo.png')
-CANVAS.create_image(10, 10, anchor='nw', image=name_img_obj)
-
-CANVAS.create_text(50, 450, text=' Score: \n123456', font=('ProtoSans56', 40),\
-                   fill='yellow', anchor='nw')
-
