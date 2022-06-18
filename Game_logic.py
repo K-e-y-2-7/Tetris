b2af51c203d595fa8563752b9eb38bef3a307dd8
Key27 <Desertod2014@mail.ua>
2022-06-18 21:58:25 +0300

    Create new file Game_logic.

---
+++ Game_logic.py
@@ -0,0 +1,12 @@
+'''
+main_game_logic.py.
+A file that reproduces the main logic of game.  
+===============================================
+
+'''
+
+from copy import deepcopy
+from random import choice, randrange
+
+from Settings import *
+
