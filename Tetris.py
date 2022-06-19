70741122418d3ff6229bb752330ebcc57ed85ab5
Key27 <Desertod2014@mail.ua>
2022-06-19 03:18:05 +0300

    moved the game startup to a file, and wrapped it in the try except block

--- Tetris.py
+++ Tetris.py
@@ -5,6 +5,27 @@ The main file of game.
 
 '''
 
+import time
+from random import randrange
 
-import tkinter
+import Exceptions
+import gui
 
+get_collor = lambda: (randrange(30, 256), randrange(30, 256), randrange(30, 256)) 
+def rgb_to_hex(rgb):
+        return '#%02x%02x%02x' % rgb
+
+
+try:
+    for item in gui.grid:
+        #time.sleep(0.3)
+        gui.canvas.itemconfigure(item, fill=rgb_to_hex(get_collor()))
+
+    for item in gui.grid:
+        gui.canvas.itemconfigure(item, fill='')
+    gui.ROOT.mainloop()
+    
+except Exceptions.GameOver:
+        ...
+except Exceptions.Quit:
+    exit()
