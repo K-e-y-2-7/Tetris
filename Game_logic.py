04579236dda6ab882693c4bab717dc90e31a4f28
Key27 <Desertod2014@mail.ua>
2022-06-21 23:43:37 +0300

    Writes all docstrings in all fuctions and writes additional comments in code

--- Game_logic.py
+++ Game_logic.py
@@ -17,28 +17,30 @@ app_running = True
 def get_score(stop=None) -> list:
     ''' Finction is extraction data from file scores.txt and
         writing them in list score.
-        Return list of score.
+
+        ======================
+        Return: list of score.
+
     '''
 
     with open('Scores.txt', 'r') as score_file_r:
-        flag = 0 
-        score = []
-        for line in score_file_r:
-            score.append(line[:-1])
-            flag += 1
-            if flag == stop: break
-
+        score = [line[:-1] for idx, line in enumerate(score_file_r)\
+                                                    if idx != stop]
+        
     return score
 
 score, lines = 0, 0
 top_10 = get_score(stop = 10)
-scores = {str(idx): score.split(': ')[2] for idx, score in enumerate(get_score())}
+scores = {str(idx): score.split(': ')[2] for idx, score in
+                                    enumerate(get_score())}
 record = int(top_10[0].split(': ')[2])
 
 # The disign of interface
 def display_top10(y = 40):
-    ''' Function display list of best 10 players with their scores
+    ''' Function display list of best 10 players with their scores.
+
     '''
+
     for player in top_10:
         top_10_canv.create_text(10, y, text=f'{player}',\
                     font=('ProtoSans56', 15), fill='orange', anchor='nw')
@@ -47,20 +49,32 @@ def display_top10(y = 40):
 
 def image_generator(img: str, size_x: int, size_y: int) -> ImageTk.PhotoImage:
     ''' Function accepts name of image, x and y size.
-        Creates image and resize it. And return image object.
+        Creates image and resize it.
+
+         ====================
+        Return: image object.
+
     '''
+
     image = ImageTk.PhotoImage(Image.open(img).resize((size_x, size_y)))
+
     return image
 
 
-def display_image(canv: Canvas, img: str, resize: tuple, coordinate: tuple, anch: str)-> ImageTk.PhotoImage:
+def display_image(canv: Canvas, img: str, resize: tuple,
+                  coordinate: tuple, anch: str)-> ImageTk.PhotoImage:
     ''' Function accepts name of image, tuple of x and y coordinate,
         canvas object, tuple of x and y size, anchor.
         Call function image_generator and display result of work image_generator.
-        Return image object.
+
+        ====================
+        Return: image object.
+
     '''
+
     image = image_generator(img, resize[0], resize[1])
     canv.create_image(coordinate[0], coordinate[1], anchor=anch, image=image)
+
     return image
 
 # Creates and display needed images
@@ -84,13 +98,13 @@ screen_canv.create_text(35, 155, text=f'Your nickname: \n {nick}',
 scr = screen_canv.create_text(35, 240, text=f'Score: {score}',
                 font=('ProtoSans56', 35), fill='orange', anchor='nw')
 
-grid_1 = [game_screen_canv.create_rectangle(x * TILE, y * TILE, x * TILE+TILE, y * TILE+TILE) for x in range(WIDTH) for y in range(HEIGHT)]
+# Creates grids for game field and scores field.
+grid_1 = [game_screen_canv.create_rectangle(
+            x * TILE, y * TILE, x * TILE+TILE, y * TILE+TILE)
+            for x in range(WIDTH) for y in range(HEIGHT)]
+
 grid_2 = [top_10_canv.create_rectangle(x, y * TILE, x * 309, y * TILE + TILE)\
                                      for x in range(2) for y in range(HEIGHT)]
-
-display_top10()
-
-
 
 figures_pos = [[((-1, 0), (-2, 0), (0, 0), (1, 0)), '#4cc9f0', 'i-fig'],
                 [((0, 0), (0, -1), (0, 1), (1, -1)), '#072ac8', 'j-fig'],
@@ -99,7 +113,10 @@ figures_pos = [[((-1, 0), (-2, 0), (0, 0), (1, 0)), '#4cc9f0', 'i-fig'],
                 [((0, 0), (-1, 0), (0, 1), (-1, -1)), '#31cb00', 's-fig'],
                 [((0, 0), (0, -1), (0, 1), (-1, 0)), '#b100e8', 't-fig'],
                 [((-1, 0), (-1, 1), (0, 0), (0, -1)), '#ff002b', 'z-fig']]
-figures = [[[[x_pos + WIDTH // 2, y_pos + 1, 1, 1] for x_pos, y_pos in fig_pos[0]], fig_pos[1], fig_pos[2]] for fig_pos in figures_pos]
+
+figures = [[[[x_pos + WIDTH // 2, y_pos + 1, 1, 1] for x_pos, y_pos in \
+                fig_pos[0]], fig_pos[1], fig_pos[2]] for fig_pos in \
+                figures_pos]
 
 field = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
 
@@ -108,32 +125,57 @@ anim_count, anim_speed, anim_limit = 0, 60, 2000
 score, lines = 0, 0
 scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
 
-get_color = lambda : (randrange(30, 256), randrange(30, 256), randrange(30, 256))
+get_color = lambda: (randrange(30, 256), randrange(30, 256),
+                                         randrange(30, 256))
 
 figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
 color, next_color = figure[1], next_figure[1]
 
-def rgb_to_hex(rgb):
+def rgb_to_hex(rgb: tuple) -> str:
+    ''' Convert color numbers to hexadecimal number.
+
+        ===========================
+        Return: hexadecimal number.
+
+    '''
+
     return '#%02x%02x%02x' % rgb
 
 
-def check_borders(i):
-    print(i)
-    if figure[0][i][0] < 0 or figure[0][i][0] > WIDTH - 1:
+def check_borders(index) -> bool:
+    ''' The function compares the position of the figure with the
+        edge of the field, and does not allow it to climb beyond the edge.
+
+        =============
+        Return: bool.
+
+    '''
+
+    if figure[0][index][0] < 0 or figure[0][index][0] > WIDTH - 1:
+
         return False
-    elif figure[0][i][1] > HEIGHT - 1 or field[figure[0][i][1]][figure[0][i][0]]:
+    elif figure[0][index][1] > HEIGHT - 1 or field[figure[0][index][1]]\
+                                                  [figure[0][index][0]]:
+
         return False
+
     return True
 
 
 def on_closing():
+    ''' Terminates the application. '''
+
     global app_running
     if messagebox.askokcancel("Выход из приложения", "Хотите выйти из приложения?"):
         app_running = False
-        #tk.destroy()
 
 
-def move_obj(event):
+def move_obj(event: str):
+    ''' Traks event, and moving or rotate figure.
+        Accept event.
+
+    '''
+
     global rotate, anim_limit, x_moving
     if event.keysym == 'Up':
         rotate = True
@@ -144,63 +186,83 @@ def move_obj(event):
     elif event.keysym == 'Right':
         x_moving = 1
 
-game_screen_canv.bind_all("<KeyPress-Up>",move_obj)
-game_screen_canv.bind_all("<KeyPress-Down>",move_obj)
-game_screen_canv.bind_all("<KeyPress-Left>",move_obj)
-game_screen_canv.bind_all("<KeyPress-Right>",move_obj)
+# Bind control keys.
+game_screen_canv.bind_all("<KeyPress-Up>", move_obj)
+game_screen_canv.bind_all("<KeyPress-Down>", move_obj)
+game_screen_canv.bind_all("<KeyPress-Left>", move_obj)
+game_screen_canv.bind_all("<KeyPress-Right>", move_obj)
 
 def move_x():
+    ''' Moves the figure along the x-axis. '''
+
     global figure
     figure_old = deepcopy(figure)
-    for i in range(4):
-        figure[0][i][0] += x_moving
-        if not check_borders(i):
+    for idx in range(4):
+        figure[0][idx][0] += x_moving
+        if not check_borders(idx):
             figure = deepcopy(figure_old)
             break
 
 
 def move_y():
+    ''' Moves the figure along the y-axis. '''
+
     global anim_count, anim_speed, anim_limit, figure
     global color, next_figure, next_color
     anim_count += anim_speed
+
     if anim_count > anim_limit:
         anim_count = 0
         figure_old = deepcopy(figure)
-        for i in range(4):
-            figure[0][i][1] += 1
-            if not check_borders(i):
-                for i in range(4):
-                    field[figure_old[0][i][1]][figure_old[0][i][0]] = color
+
+        for idx in range(4):
+            figure[0][idx][1] += 1
+            if not check_borders(idx):
+
+                for idx in range(4):
+                    field[figure_old[0][idx][1]]\
+                            [figure_old[0][idx][0]] = color
                 figure, color = next_figure, next_color
                 next_figure  = deepcopy(choice(figures))
                 next_color = next_figure[1]
                 anim_limit = 2000
                 break
 
+
 def overturn():
+    ''' Rotates the figure along the x-axis. '''
+
     global figure
-    center = figure[0][0]
+    center = figure[0][0]  # first element figure
     figure_old = deepcopy(figure)
     if rotate:
-        for i in range(4):
-            x = figure[0][i][1] - center[1]
-            y = figure[0][i][0] - center[0]
-            figure[0][i][0] = center[0] - x
-            figure[0][i][1] = center[1] + y
-            if not check_borders(i):
+        for idx in range(4):
+            x_pos = figure[0][idx][1] - center[1]
+            y_pos = figure[0][idx][0] - center[0]
+            figure[0][idx][0] = center[0] - x_pos
+            figure[0][idx][1] = center[1] + y_pos
+            if not check_borders(idx):
                 figure = deepcopy(figure_old)
                 break
 
 
-def check_lines():
+def check_lines() -> int:
+    ''' The function checks whether a line of shapes has been created
+        at the bottom of the field. And counts these lines.
+
+        ==============
+        Return: lines.
+
+    '''
+
     global anim_speed
     line, lines = HEIGHT - 1, 0
     for row in range(HEIGHT - 1, -1, -1):
         count = 0
-        for i in range(WIDTH):
-            if field[row][i]:
+        for idx in range(WIDTH):
+            if field[row][idx]:
                 count += 1
-            field[line][i] = field[row][i]
+            field[line][idx] = field[row][idx]
         if count < WIDTH:
             line -= 1
         else:
@@ -210,9 +272,15 @@ def check_lines():
     return lines
 
 
-def draw_figure(fig_list):
+def draw_figure(fig_list) -> list:
     ''' Function displays a figure on the game field.
+        Accept list, append figure that was displayed in list.
+
+        =============
+        Return: list.
+
     '''
+
     for idx in range(4):
         figure_rect_x = figure[0][idx][0] * TILE 
         figure_rect_y = figure[0][idx][1] * TILE
@@ -242,27 +310,47 @@ nxt_figs_img = {
     'z-fig': Image.open('img/Z_figure.png').resize((93, 140)) }
 
 
-def draw_next_figure(fig_list):
+def draw_next_figure(fig_list: list) -> list:
     ''' Function displays on screen which figure will be next.
+        Accept list, append figure that was displayed in list.
+
+        =============
+        Return: list.
+
     '''
+
     global nxt_fig_img
+    
     nxt_fig_img = ImageTk.PhotoImage(nxt_figs_img[next_figure[2]])
-    fig_list.append(screen_canv.create_image(180, 450, anchor='center', image=nxt_fig_img))
+    fig_list.append(screen_canv.create_image(180, 450, anchor='center',
+                                                  image=nxt_fig_img))
 
     return fig_list
 
 
-def draw_field(fig_list):
+def draw_field(fig_list: list) -> list:
+    ''' Function displays a figure on the game field.
+        Displays fallen figures. Accept list, append 
+        figure that was displayed in list.
+
+        =============
+        Return: list.
+
+    '''
+
     for y, raw in enumerate(field):
         for x, col in enumerate(raw):
             if col:
                 figure_rect_x, figure_rect_y = x * TILE, y * TILE
-                fig_list.append(game_screen_canv.create_rectangle(figure_rect_x, figure_rect_y, figure_rect_x + TILE,
-                                                    figure_rect_y + TILE, fill=col))
+                fig_list.append(game_screen_canv.create_rectangle(
+                                figure_rect_x, figure_rect_y, figure_rect_x +
+                                TILE, figure_rect_y + TILE, fill=col))
+
     return fig_list
 
 
 def game_over():
+    '''Function determines behavior after the end of the game'''
     global field
     for i in range(WIDTH):
         if field[0][i]:
@@ -270,7 +358,8 @@ def game_over():
             anim_count, anim_speed, anim_limit = 0, 60, 2000
             score = 0
             for item in grid_1:
-                game_screen_canv.itemconfigure(item, fill=rgb_to_hex(get_color()))
+                game_screen_canv.itemconfigure(item,
+                                        fill=rgb_to_hex(get_color()))
                 time.sleep(0.005)
                 tetris.update_idletasks()
                 tetris.update()
@@ -279,6 +368,8 @@ def game_over():
 
 x_moving, rotate = 0, False
 def start():
+    '''Launches the program'''
+    
     global score, x_moving, rotate
     while app_running:
         if app_running:
@@ -289,32 +380,32 @@ def start():
             # Rotation of our figure around its axis.
             overturn()  
             lines = check_lines()
-
             # Compute score
             score += scores[lines]
-
             fig1, fig2 = [], []
             # Displays a figure on the game field.
             fig1 = draw_figure(fig1)
-
-            # draw field
+            # Displays a game field.
             fig1 += draw_field(fig1)
-
-            # draw next figure
+            # Displays a next figure.
             fig2 = draw_next_figure(fig2)
-
+            # Displays a changing in score and record
             screen_canv.itemconfigure(scr, text=f'Score: {score}')
             screen_canv.itemconfigure(rec, text=f'BEST RECORD:  {record}')
+            display_top10()
 
             # game over
             game_over()
-
+            # Set default values for next iteration. 
             x_moving, rotate = 0, False
+
             tetris.update_idletasks()
             tetris.update()
+            # Delete the figure after moving.
             for id_fig in fig1: game_screen_canv.delete(id_fig)
+            # Remove from the following figures, the current one.
             for id_fig in fig2: screen_canv.delete(id_fig)
         time.sleep(0.005)
 
-
+start()
 tetris.destroy()
