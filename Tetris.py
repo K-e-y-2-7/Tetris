46953643dc20d4134739bab490e7637e53b71274
Key27 <Desertod2014@mail.ua>
2022-06-24 05:39:53 +0300

    Removed unnecesary exception

--- Tetris.py
+++ Tetris.py
@@ -5,14 +5,11 @@ The main file of game.
 
 '''
 
-import Exceptions
 import Game_logic
 
 
 try:    
     Game_logic.tetris.mainloop()  
-except Exceptions.GameOver:
-    print(Exceptions.GameOver)
 except FileNotFoundError:
     with open('Scores.txt', 'w') as score_file_w:
         pass 
