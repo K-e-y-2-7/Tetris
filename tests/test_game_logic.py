8c2b2ded818fc47313c30c5987f7ebd37e920c5a
Key27 <Desertod2014@mail.ua>
2022-07-02 13:56:44 +0300

    Writes doc string of files and import files for testing.

---
+++ tests/test_game_logic.py
@@ -0,0 +1,8 @@
+''' This file contains tests for the Game_logic.py file.
+    It tests all functions in Game_logic.py file.
+
+'''
+
+import unittest
+
+import Game_logic
---
+++ tests/test_gui.py
@@ -0,0 +1,8 @@
+''' This file contains tests for the Gui.py file.
+    It tests all functions in Gui.py file.
+
+'''
+
+import unittest
+
+import Gui
